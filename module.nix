8f90d44fd62b62831a5967d01144a66e00090890
Jean-François Roche <jfroche@pyxel.be>
2023-03-28 12:56:21 +0200

    Add nixos module

---
+++ module.nix
@@ -0,0 +1,100 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.queued-build-hook;
+  queued-build-hook = pkgs.callPackage ./. { };
+in
+{
+  options.queued-build-hook = {
+    enable = lib.mkEnableOption "queued-build-hook";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = queued-build-hook;
+      description = lib.mdDoc ''
+        The queued-build-hook package to use.
+      '';
+    };
+
+    socketDirectory = mkOption {
+      description = lib.mdDoc ''
+        Path to store the queued-build-hook daemon's unix socket.
+      '';
+      default = "/var/lib/nix";
+      type = types.path;
+    };
+
+    socketUser = mkOption {
+      type = types.str;
+      example = "user";
+      default = "root";
+      description = lib.mdDoc ''
+        This users will have read/write access to the Unix socket.
+      '';
+    };
+
+    socketGroup = mkOption {
+      description = lib.mdDoc ''
+        The users in this group will have read/write access to the Unix socket.
+      '';
+      type = types.str;
+      default = "nixbld";
+    };
+
+    postBuildScriptContent = mkOption {
+      description = lib.mdDoc ''
+        Specify the content of the script that will manage the newly built package.
+        The script must be able to handle the OUT_PATHS environment variable, which contains a list of the paths to the newly built packages.
+      '';
+
+      example = literalExpression ''
+        exec nix copy --experimental-features nix-command --to "file:///var/nix-cache" $OUT_PATHS
+      '';
+      type = types.str;
+    };
+
+  };
+  config = lib.mkIf cfg.enable {
+
+    nix.settings.post-build-hook =
+      let
+        enqueueScript = pkgs.writeShellScriptBin "enqueue-package" ''
+          ${cfg.package}/bin/queued-build-hook queue --socket "${cfg.socketDirectory}/async-nix-post-build-hook.sock"
+        ''; in
+      "${enqueueScript}/bin/enqueue-package";
+
+    systemd.sockets = {
+      async-nix-post-build-hook = {
+        description = "Async nix post build hooks socket";
+        wantedBy = [ "sockets.target" ];
+        socketConfig = {
+          ListenStream = "${cfg.socketDirectory}/async-nix-post-build-hook.sock";
+          SocketMode = "0660";
+          SocketUser = cfg.socketUser;
+          SocketGroup = cfg.socketGroup;
+          Service = "async-nix-post-build-hook.service";
+        };
+      };
+    };
+
+    systemd.services =
+      let
+        hook = pkgs.writeShellScript "hook" cfg.postBuildScriptContent;
+      in
+      {
+        async-nix-post-build-hook = {
+          description = "Run nix post build hooks asynchronously";
+          wantedBy = [ "multi-user.target" ];
+          requires = [
+            "async-nix-post-build-hook.socket"
+          ];
+          serviceConfig = {
+            Type = "notify";
+            ExecStart = "${cfg.package}/bin/queued-build-hook daemon --hook ${hook}";
+            RestrictAddressFamilies = "AF_UNIX";
+            FileDescriptorStoreMax = 1;
+          };
+        };
+      };
+  };
+}
--- test.nix
+++ test.nix
@@ -1,59 +1,24 @@
 { pkgs ? import <nixpkgs> { }
 , lib ? pkgs.lib
 }:
-let
-  queued-build-hook = import ./. { };
-  dummy-hook = pkgs.writeShellScriptBin "dummy-hook" ''
-    # This is a dummy post-build-hook that copy over derivation to another directory
-    mkdir -p /var/nix-cache
-    echo "Uploading paths" $OUT_PATHS
-    exec ${pkgs.nix}/bin/nix copy --experimental-features nix-command --to "file:///var/nix-cache" $OUT_PATHS
-  '';
-  socketPath = "/var/lib/nix/async-nix-post-build-hook.socket";
-  enqueue-package = pkgs.writeShellScriptBin "enqueue-package" ''
-    ${queued-build-hook}/bin/queued-build-hook queue --socket ${socketPath}
-  '';
-in
 pkgs.nixosTest
 {
   name = "queued-build-hook";
   nodes = {
     ci = { ... }: {
-      nix.settings.post-build-hook = "${enqueue-package}/bin/enqueue-package";
+      imports = [ ./module.nix ];
+
+      queued-build-hook = {
+        enable = true;
+        postBuildScriptContent = ''
+          # This is a dummy post-build-hook that copy over derivation to another directory
+          mkdir -p /var/nix-cache
+          echo "Uploading paths" $OUT_PATHS
+          exec ${pkgs.nix}/bin/nix copy --experimental-features nix-command --to "file:///var/nix-cache" $OUT_PATHS
+        '';
+      };
 
       system.extraDependencies = with pkgs; [ hello.inputDerivation ];
-
-      systemd.tmpfiles.rules = [
-        "d /var/nix-cache 0770 root nixbld - -"
-      ];
-
-      systemd.sockets = {
-        async-nix-post-build-hook = {
-          description = "Async nix post build hooks socket";
-          wantedBy = [ "sockets.target" ];
-          socketConfig = {
-            ListenStream = socketPath;
-            SocketMode = "0660";
-            SocketGroup = "nixbld";
-            Service = "async-nix-post-build-hook.service";
-          };
-        };
-      };
-      systemd.services = {
-        async-nix-post-build-hook = {
-          description = "Run nix post build hooks asynchronously";
-          wantedBy = [ "multi-user.target" ];
-          requires = [
-            "async-nix-post-build-hook.socket"
-          ];
-          serviceConfig = {
-            Type = "notify";
-            ExecStart = "${queued-build-hook}/bin/queued-build-hook daemon --hook ${dummy-hook}/bin/dummy-hook";
-            RestrictAddressFamilies = "AF_UNIX";
-            FileDescriptorStoreMax = 1;
-          };
-        };
-      };
     };
 
   };

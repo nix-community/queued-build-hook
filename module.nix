e7bb8e9f203ccac1b25fd808232dcd5151c87e8c
Jean-François Roche <jfroche@pyxel.be>
2023-04-03 15:00:20 +0200

    feat(nixos module): add daemon options and client script options
    Enable user to customize the enqueue script and the daemon's concurrency and error handling options

--- module.nix
+++ module.nix
@@ -41,12 +41,48 @@ in
       default = "nixbld";
     };
 
+    retryInterval = mkOption {
+      description = lib.mdDoc ''
+        The number of seconds between attempts to run the hook for a package after an initial failure.
+      '';
+      type = types.int;
+      default = 1;
+    };
+
+    retries = mkOption {
+      description = lib.mdDoc ''
+        The maximum number of attempts that will be made to run the hook for a package before giving up and dropping the task altogether.
+      '';
+      type = types.int;
+      default = 5;
+    };
+
+    concurrency = mkOption {
+      description = lib.mdDoc ''
+        Sets the maximum number of tasks that can be executed simultaneously.
+        By default it is set to 0 which means there is no limit to the number of tasks that can be run concurrently.
+      '';
+      type = types.int;
+      default = 0;
+    };
+
+    enqueueScriptContent = mkOption {
+      description = lib.mdDoc ''
+        The script's content responsible for enqueuing newly-built packages and passing them to the daemon.
+        Although the default configuration should suffice, there may be situations that require customized handling of specific packages.
+        For example, it may be necessary to process certain packages synchronously using the 'queued-build-hook wait' command, or to ignore certain packages entirely.
+      '';
+      default = ''
+        ${cfg.package}/bin/queued-build-hook queue --socket "${cfg.socketDirectory}/async-nix-post-build-hook.sock"
+      '';
+      type = types.str;
+    };
+
     postBuildScriptContent = mkOption {
       description = lib.mdDoc ''
         Specify the content of the script that will manage the newly built package.
         The script must be able to handle the OUT_PATHS environment variable, which contains a list of the paths to the newly built packages.
       '';
-
       example = literalExpression ''
         exec nix copy --experimental-features nix-command --to "file:///var/nix-cache" $OUT_PATHS
       '';
@@ -58,9 +94,7 @@ in
 
     nix.settings.post-build-hook =
       let
-        enqueueScript = pkgs.writeShellScriptBin "enqueue-package" ''
-          ${cfg.package}/bin/queued-build-hook queue --socket "${cfg.socketDirectory}/async-nix-post-build-hook.sock"
-        '';
+        enqueueScript = pkgs.writeShellScriptBin "enqueue-package" cfg.enqueueScriptContent;
       in
       "${enqueueScript}/bin/enqueue-package";
 
@@ -90,7 +124,10 @@ in
             "async-nix-post-build-hook.socket"
           ];
           serviceConfig = {
-            ExecStart = "${cfg.package}/bin/queued-build-hook daemon --hook ${hook}";
+            ExecStart = "${cfg.package}/bin/queued-build-hook daemon --hook ${hook} " +
+              "--retry-interval ${toString cfg.retryInterval} " +
+              "--retry-interval ${toString cfg.retries} " +
+              "--concurrency ${toString cfg.concurrency}";
             FileDescriptorStoreMax = 1;
           };
         };
